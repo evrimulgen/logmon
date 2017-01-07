30804e757a66eef1a9bacae8cc809a365cc136a5
Gabin Marignier <gabin.marignier@gmail.com>
2017-01-07 16:41:42 +0100

    Read from log file using tail approach

--- main.go
+++ main.go
@@ -4,19 +4,29 @@ import (
     "log"
     "strconv"
     "os"
-)
 
-func usageAndQuit() {
-   log.Fatalln("Usage:\n\n\tlogmon [logPath] [threshold]\n")
-}
+    "github.com/hpcloud/tail"
+)
 
 func main() {
     if len(os.Args) != 3 {
-        usageAndQuit()
+        log.Fatalln("Usage:\n\n\tlogmon [logPath] [threshold]\n")
     }
     logPath := os.Args[1]
     threshold, err := strconv.Atoi(os.Args[2])
     if err != nil {
         log.Fatalln(err)
     }
+    println(threshold)
+
+    t, err := tail.TailFile(logPath, tail.Config{
+		Follow:   true,
+		ReOpen:   true,
+	})
+	if err != nil {
+        log.Fatalln(err)
+	}
+	for line := range t.Lines {
+        println(line.Text)
+	}
 }
