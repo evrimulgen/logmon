962934918bb7aa63185c327e568545bc1dbe4c05
Gabin Marignier <gabin.marignier@gmail.com>
2017-01-09 02:50:18 +0100

    Add a unit test for testing Alert logic

---
+++ models/models_test.go
@@ -0,0 +1,40 @@
+package models
+
+import (
+	"testing"
+)
+
+func TestAlertLogic(t *testing.T) {
+    cb := NewCircularBuffer()
+    cb.checkAlert(20)
+    // No hit yet, so no alert should be generated
+    if cb.alert == true {
+        t.Error()
+    }
+
+    cb.periods.Value = &Period{make(map[string]uint64), 30}
+    cb.checkAlert(20)
+    // An alert should be generated here because a period registered 
+    // more hits than the threshold
+    if cb.alert == false {
+        t.Error()
+    }
+
+    cb.periods = cb.periods.Next()
+    cb.periods.Value = &Period{make(map[string]uint64), 10}
+    cb.checkAlert(20)
+    // The alert should still be there because the total number of hits 
+    // is now 40, which is above the threshold
+    if cb.alert == false {
+        t.Error()
+    }
+
+    cb.periods = cb.periods.Prev()
+    cb.periods.Value = &Period{make(map[string]uint64), 5}
+    cb.checkAlert(20)
+    // The alert should be stopped because the total number of hits
+    // is now of 15, which is below the threshold
+    if cb.alert == true {
+        t.Error()
+    }
+}
