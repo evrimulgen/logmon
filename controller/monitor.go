e6a61f99d7b46cb0bb2d433d134c1c912f4fef4c
Gabin Marignier <gabin.marignier@gmail.com>
2017-01-09 01:23:19 +0100

    Correct a bug while reseting CircularBuffer

--- controller/monitor.go
+++ controller/monitor.go
@@ -8,9 +8,7 @@ import (
 )
 
 func Monitor(cb *models.CircularBuffer) {
-    var period_counter uint
     for _ = range time.Tick(config.PERIOD) {
-        period_counter += 1
-        cb.DisplaySectionsMostHitAndNext(&period_counter)
+        cb.DisplayStatsAndNext()
     }
 }
--- models/circularbuffer.go
+++ models/circularbuffer.go
@@ -4,58 +4,57 @@ import (
 	"container/ring"
     "sync"
     "fmt"
-
-    "github.com/gabsn/logmon/config"
 )
 
 // Data structure holding hits information for the last 2 minutes
 type CircularBuffer struct {
     sync.Mutex
 	periodHits *ring.Ring
-	totalHits map[string]int
+	totalHits map[string]uint
 }
 
 // Returns a new initialized circular buffer
 func NewCircularBuffer(nbPeriod int) *CircularBuffer {
 	r := ring.New(nbPeriod)
 	for i := 0; i < r.Len(); i++ {
-		r.Value = make(map[string]int)
+		r.Value = make(map[string]uint)
 		r = r.Next()
 	}
-	return &CircularBuffer{sync.Mutex{}, r, make(map[string]int)}
+	return &CircularBuffer{sync.Mutex{}, r, make(map[string]uint)}
 }
 
 // Increments the counter of hits
 func (cb *CircularBuffer) HitBy(h Hit) {
     cb.Lock()
     //fmt.Println("Hitting section:", h.Section)
-    cb.periodHits.Value.(map[string]int)[h.Section] += 1
+    cb.periodHits.Value.(map[string]uint)[h.Section] += 1
     cb.totalHits[h.Section] += 1
     cb.Unlock()
 }
 
 // Display sections most hit during the last 10s and next Period
-func (cb *CircularBuffer) DisplaySectionsMostHitAndNext(period_counter *uint) {
+func (cb *CircularBuffer) DisplayStatsAndNext() {
     cb.Lock()
+    cb.displayStats()
+    cb.periodHits = cb.periodHits.Next()
+    cb.periodHits.Value = make(map[string]uint)
+    cb.Unlock()
+}
+
+func (cb *CircularBuffer) displayStats() {
     fmt.Println("Sections most hit during the last 10s:")
-    hits := cb.periodHits.Value.(map[string]int)
+    hits := cb.periodHits.Value.(map[string]uint)
     for k, v := range hits {
         fmt.Printf("\t-> %s: %v hits\n",  k, v)
     }
     fmt.Println()
-    cb.periodHits = cb.periodHits.Next()
-    if *period_counter == config.NB_PERIOD {
-        cb.reset()
-        *period_counter = 0
-    }
-    cb.Unlock()
 }
 
 // Reset the period hits counters (no mutexes because it's a private method)
 func (cb *CircularBuffer) reset() {
     r := cb.periodHits
     for i := 0; i < r.Len(); i++ {
-		r.Value = make(map[string]int)
+		r.Value = make(map[string]uint)
 		r = r.Next()
 	}
 }
